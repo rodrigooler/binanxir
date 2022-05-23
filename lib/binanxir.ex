073d23946d2045c271aa5aa856d71504bcdc2dd4
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2022-05-23 02:21:28 -0300

    chore: create project

---
+++ lib/binanxir.ex
@@ -0,0 +1,18 @@
+defmodule Binanxir do
+  @moduledoc """
+  Documentation for `Binanxir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Binanxir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/binanxir_test.exs
@@ -0,0 +1,8 @@
+defmodule BinanxirTest do
+  use ExUnit.Case
+  doctest Binanxir
+
+  test "greets the world" do
+    assert Binanxir.hello() == :world
+  end
+end
